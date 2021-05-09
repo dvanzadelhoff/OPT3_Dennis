d5b69e58a15295850dfdf5a6b0a541b136d9a399
dvanzadelhoff <dvanzadelhoff2412@gmail.com>
2021-05-09 23:32:13 +0200

    added new branch: FEATURE-users

--- src/com/company/Users.java
+++ src/com/company/Users.java
@@ -2,4 +2,42 @@ package com.company;
 
 public class Users {
 
+    private String naam;
+    private int leeftijd;
+    private boolean versie;
+    private boolean actief;
+    private String systeem;
+
+    public Users(String naam, int leeftijd, boolean versie, boolean actief, String systeem)
+    {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.versie = versie;
+        this.actief = actief;
+        this.systeem = systeem;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    public boolean isVersie() {
+        return versie;
+    }
+
+    public boolean isActief() {
+        return actief;
+    }
+
+    public String getSysteem() {
+        return systeem;
+    }
+
+    public Double berekenPrijs() {
+
+    }
 }
