2b9ee3bd052459a60954cce90bbf9526cc1ac8b1
Dennis van Zadelhoff <dvanzadelhoff2412@gmail.com>
2021-05-26 21:22:43 +0200

    new test file successfull: pairwiseTest

--- src/com/company/Users.java
+++ src/com/company/Users.java
@@ -2,4 +2,71 @@ package com.company;
 
 public class Users {
 
+    private String naam;
+    private int leeftijd;
+    private boolean versie;
+    private boolean actief;
+    private String systeem;
+
+    public Users(String naam, int leeftijd, boolean versie, boolean actief, String systeem)
+    {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.versie = versie;
+        this.actief = actief;
+        this.systeem = systeem;
+    }
+
+    public String getNaam()
+    {
+        return this.naam;
+    }
+
+    public int getLeeftijd()
+    {
+        return this.leeftijd;
+    }
+
+    public boolean isVersie()
+    {
+        return this.versie;
+    }
+
+    public boolean isActief()
+    {
+        return this.actief;
+    }
+
+    public String getSysteem()
+    {
+        return this.systeem;
+    }
+
+    public Double berekenPrijs()
+    {
+        Double prijs = 5.0;
+
+        if (this.versie)
+        {
+            prijs = 10.0;
+
+            if (this.leeftijd <= 21)
+            {
+                prijs = prijs - 2.0;
+            }
+
+            if (this.leeftijd >= 65)
+            {
+                prijs = prijs - 1.0;
+            }
+        }
+
+        if (this.systeem == "MacOs")
+        {
+            prijs = prijs + 3;
+        }
+
+        return prijs;
+    }
+
 }
--- test/com/company/pairwiseTest.java
+++ test/com/company/pairwiseTest.java
@@ -4,11 +4,32 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class pairwiseTest
-{
+public class pairwiseTest {
     @Test
-    public void pairwiseTesting()
-    {
+    public void pairwiseTesting1() {
+        Users test1 = new Users("Dennis1", 20, false, true, "Windows");
+        assertEquals(5.0, test1.berekenPrijs(), 0.001);
 
+        Users test2 = new Users("Dennis2", 21, true, false, "MacOs");
+        assertEquals(11.0, test2.berekenPrijs(), 0.001);
+
+    }
+
+    @Test
+    public void pairwiseTesting2() {
+        Users test1 = new Users("Barend1", 22, false, false, "MacOs");
+        assertEquals(8.0, test1.berekenPrijs(), 0.001);
+
+        Users test2 = new Users("Barend2", 64, true, true, "Windows");
+        assertEquals(10.0, test2.berekenPrijs(), 0.001);
+    }
+
+    @Test
+    public void pairwiseTesting3() {
+        Users test1 = new Users("andre1", 65, false, false, "Windows");
+        assertEquals(5.0, test1.berekenPrijs(), 0.001);
+
+        Users test2 = new Users("andre2", 66, true, true, "MacOs");
+        assertEquals(12.0, test2.berekenPrijs(), 0.001);
     }
 }
